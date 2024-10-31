12f354a311ca091b78fc3a21852cfa24aa31f8b9
namgyuri <skarbfl0802@gmail.com>
2024-10-31 13:02:01 +0900

    init

---
+++ src/main/java/com/luckython/Pinmate/LuckythonApplication.java
@@ -0,0 +1,13 @@
+package com.luckython.Pinmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckythonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckythonApplication.class, args);
+	}
+
+}
